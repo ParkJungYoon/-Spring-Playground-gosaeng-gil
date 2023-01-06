ace0666313cfd0b0549557dd224e4b14fb11a6ff
ParkJungYoon <tbr06057@naver.com>
2023-01-06 20:41:05 +0900

    싱글톤 방식의 주의점-수정 버전

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -2,15 +2,20 @@ package hello.core.singleton;
 
 public class StatefulService {
 
-    private int price; // 상태를 유지하는 필드
+//    private int price; // 상태를 유지하는 필드
 
-    public void order(String name, int price) {
+    // 상태를 유지하기 때문에 이렇게 하면 안된다.
+//    public void order(String name, int price) {
+//        System.out.println("name = " + name + "price = " + price);
+//        this.price = price;
+//    }
+
+    public int order(String name, int price) {
         System.out.println("name = " + name + "price = " + price);
-        this.price = price;
-    }
-
-    public int getPrice() {
         return price;
     }
+//    public int getPrice() {
+//        return price;
+//    }
 }
 

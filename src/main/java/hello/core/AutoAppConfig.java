3fecc721058b612dc597b23676ebd6fbdaaf024a
ParkJungYoon <tbr06057@naver.com>
2023-01-12 00:55:24 +0900

    @Qualifier 사용해서 조회 빈 대상이 2개 이상인 경우의 문제점 해결

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.FilterType;
 // 기존 예제 코드를 유지하기 위해서 이렇게 제외하고 시작
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member",
+//        basePackages = "hello.core.member",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {

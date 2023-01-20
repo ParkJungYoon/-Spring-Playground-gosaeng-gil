781ac2c355c1fc107972da60a296c97e3988289f
ParkJungYoon <tbr06057@naver.com>
2023-01-20 16:13:52 +0900

    HttpServletRequest - 기본 사용법

--- src/main/java/hello/servlet/basic/HelloServlet.java
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
 
+    // 단축키 ctrl + O
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 

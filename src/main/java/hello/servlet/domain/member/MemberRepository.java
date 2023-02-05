ef5e9f661d70716a76994f1dd07940f8d31b09e0
ParkJungYoon <tbr06057@naver.com>
2023-02-05 17:53:42 +0900

    View 분리 - v2

--- src/main/java/hello/servlet/domain/member/MemberRepository.java
+++ src/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public class MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
-    private static long sequence = 8L;
+    private static long sequence = 0L;
 
     private static final MemberRepository instance = new MemberRepository();
 
